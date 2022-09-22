ae40bcde4a5e2d140739fb8162438a82db845e93
Virender Singh Rathore <90162515+VirennnR@users.noreply.github.com>
2022-09-22 22:21:49 +0530

    Add files via upload
    Virender Singh Rathore
    GDSC Compose Camp Day 3
    Meditation App

---
+++ Submissions/Day3/MeditationApp/app/src/main/java/com/example/meditationapp/ui/theme/BottomBarItemsProperties.kt
@@ -0,0 +1,10 @@
+package com.example.meditationapp.ui.theme
+
+import androidx.annotation.DrawableRes
+
+data class BottomBarItemsProperties(
+
+    val iconname: String,
+    @DrawableRes val icon: Int
+)
+
---
+++ Submissions/Day3/MeditationApp/app/src/main/java/com/example/meditationapp/ui/theme/GridProperties.kt
@@ -0,0 +1,10 @@
+package com.example.meditationapp.ui.theme
+
+import androidx.annotation.DrawableRes
+import androidx.compose.ui.graphics.Color
+
+data class GridProperties(
+    val title: String,
+    @DrawableRes val Icon : Int,
+    val squareColor: Color
+)
